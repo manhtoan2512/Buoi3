577908324a1c42e0647a03c39f6b9afa2aae22ef
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-06 16:46:16 +0700

    Create Bai12.java

---
+++ src/BaiVeNha/Bai12.java
@@ -0,0 +1,13 @@
+package BaiVeNha;
+
+import java.util.Scanner;
+
+public class Bai12 {
+    public static void main(String[] args) {
+        System.out.print("Nhập m: ");
+        float m = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập n: ");
+        float n = new Scanner(System.in).nextFloat();
+        
+    }
+}
