421120ad10f8a8cd8e40ea0543cd60c18fe2981f
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-06 12:10:37 +0700

    Update Bai01.java

--- src/BaiVeNha/Bai01.java
+++ src/BaiVeNha/Bai01.java
@@ -1,7 +1,26 @@
 package BaiVeNha;
 
+import java.util.Scanner;
+
 public class Bai01 {
     public static void main(String[] args) {
-        int a = 0;
+        System.out.print("Nhập a: ");
+        float a = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập b: ");
+        float b = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập c: ");
+        float c = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập d: ");
+        float d = new Scanner(System.in).nextFloat();
+
+        if (a > b && a > c && a > d) {
+            System.out.println("a là số lớn nhất");
+        } else if (b>c && b>a && b>d) {
+            System.out.println("b là số lớn nhất");
+        } else if (c>a && c>b && c>d) {
+            System.out.println("c là số lớn nhất");
+        } else {
+            System.out.println("d là số lớn nhất");
+        }
     }
 }
