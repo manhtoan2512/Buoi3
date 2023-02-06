aa076328b2335ad40e46f5ffe8b7fcdf7ca7e93e
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-06 13:02:43 +0700

    Update Bai02.java

--- src/BaiVeNha/Bai02.java
+++ src/BaiVeNha/Bai02.java
@@ -6,10 +6,14 @@ public class Bai02 {
     public static void main(String[] args) {
         System.out.print("Nhập số tự nhiên n: ");
         int n = new Scanner(System.in).nextInt();
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        int j = 0;
+        for (i = 1; i <= n; i++) {
             if (n % i == 0) {
                 System.out.println("ước của n là: " + i);
+                j++;
             }
         }
+        System.out.println("n có " + j + " ước số");
     }
 }
