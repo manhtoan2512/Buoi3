48e765888a138efcaf931ad3b4a8e6ebcbed2562
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-06 15:46:46 +0700

    Update Bai08.java

--- src/BaiVeNha/Bai08.java
+++ src/BaiVeNha/Bai08.java
@@ -11,9 +11,9 @@ public class Bai08 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if(i == 1 || i == n || j == 1 || j == m) {
-                    System.out.print("* ");
+                    System.out.print("*  ");
                 }else {
-                    System.out.print("  ");
+                    System.out.print("   ");
                 }
             }
             System.out.println();
