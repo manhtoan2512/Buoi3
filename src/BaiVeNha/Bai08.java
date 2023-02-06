7c0fb0c8408175511211797a0243e106f90e11da
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-06 15:41:15 +0700

    Create Bai08.java

---
+++ src/BaiVeNha/Bai08.java
@@ -0,0 +1,22 @@
+package BaiVeNha;
+
+import java.util.Scanner;
+
+public class Bai08 {
+    public static void main(String[] args) {
+        System.out.print("Nhập n: ");
+        int n = new Scanner(System.in).nextInt();
+        System.out.print("Nhập m: ");
+        int m = new Scanner(System.in).nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if(i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
